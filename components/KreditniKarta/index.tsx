5f1c5e190e6b256142c05a9f8ad27ff79682e910
stitaks <sarka.stitak@gmail.com>
2024-07-03 20:34:24 +0200

    hotovo

--- components/KreditniKarta/index.tsx
+++ components/KreditniKarta/index.tsx
@@ -1,15 +1,35 @@
-import React, { useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 export const KreditniKarta : React.FC = () => {
 
-    const cardInputRef = useRef<HTMLInputElement>(null);
+
+    const cardInputRef1 = useRef<HTMLInputElement>(null!);
+    const cardInputRef2 = useRef<HTMLInputElement>(null!);
+    const cardInputRef3 = useRef<HTMLInputElement>(null!);
+    const cardInputRef4 = useRef<HTMLInputElement>(null!);
+
+    useEffect(() => {
+      cardInputRef1.current.focus();
+    }, []);
+
+    const onChange = () => {
+      if (cardInputRef1.current?.value?.length === 4) {
+        cardInputRef2.current.focus();
+      }
+      if (cardInputRef2.current?.value?.length === 4) {
+        cardInputRef3.current.focus();
+      }
+      if (cardInputRef3.current?.value?.length === 4) {
+        cardInputRef4.current.focus();
+      }
+    }
 
   return (
     <>
-      <input type="number" id="firstnumber" />
-      <input type="number" id="secondnumber" />
-      <input type="number" id="thirdnumber" />
-      <input type="number" id="fourthnumber" />
+      <input type="number" ref={cardInputRef1} onChange={onChange}/> 
+      <input type="number" ref={cardInputRef2} onChange={onChange}/> 
+      <input type="number" ref={cardInputRef3} onChange={onChange}/> 
+      <input type="number" ref={cardInputRef4} onChange={onChange}/> 
     </>
   );
 };
